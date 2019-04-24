4ea91ccaa3edfb5854c3309ef0767e01d453b579
stanislawkostenko <rng92@mail.ru>
2019-04-24 23:59:40 +0300

    Did first task

--- static/main.js
+++ static/main.js
@@ -1,40 +1,64 @@
 class Profile {
 
-    constructor(name, surname, country, birth) {
-        this.name = name;
-        this.surname = surname;
-        this.country = country;
+    // constructor(name, surname, country, birth) {
+    //     this.name = name;
+    //     this.surname = surname;
+    //     this.country = country;
+    //
+    //     birth = new Date().getFullYear();
+    //     const age = new Date().getFullYear() - birth;
+    //     this.age = age;
+    // }
 
-        birth = new Date().getFullYear();
-        const age = new Date().getFullYear() - birth;
-        this.age = age;
+    addNewUser(callback) {
+		    return ApiConnector.createUser(
+            {
+						    username: this.username,
+						    name: this.name,
+						    password: this.password
+				    },
+				    (err, data) => {
+						    console.log(`Creating user ${this.username}`);
+						    callback(err, data);
+				    }
+		    );
     }
 
-    addNewUser() {
-
+    authorization({ username, password }, callback) {
+        return ApiConnector.performLogin(
+            {
+                username: this.username,
+                password: this.password
+            },
+            (err, data) => {
+                console.log(`Authorizing user ${this.username}`);
+                callback(err, data);
+            }
+        );
     }
 
-    authorization() {
-
+    addMoney({ currency, amount }, callback) {
+        return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+            console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+            callback(err, data);
+        });
     }
 
-    addMoney() {
-
+    convertingMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
+        return ApiConnector.convertMoney({fromCurrency, targetCurrency, targetAmount}, (err, data) => {
+            console.log(`Converting ${fromCurrency} to ${targetAmount} ${targetCurrency}`);
+            callback(err, data);
+        });
     }
 
-    convertingMoney() {
-
-    }
-
-    sendMoney() {
-
-    }
-
-    returnExchangeRate() {
-
+    sendMoney({ to, amount }, callback) {
+        return ApiConnector.transferMoney({ to, amount}, (err, data) => {
+            console.log(`Transfering ${amount} of Netcoins to ${to}`);
+            callback(err, data);
+        });
     }
 
 }
-
-const sguclicky = new Profile('Stanislaw', 'Guslicky', 'Russia', (1992, 0, 4));
-console.log(sguclicky);
+//
+// const sguclicky = new Profile('Stanislaw', 'Guslicky', 'Russia');
+// console.log(sguclicky);
